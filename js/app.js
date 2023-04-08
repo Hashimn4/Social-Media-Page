5a6701b74791bd789d1547929916cd39751acecd
Hashim Nadeem <hashimnadeem04@gmail.com>
2023-04-08 17:37:29 -0400

    Pushing JavaScript code to github.

--- js/app.js
+++ js/app.js
@@ -7,8 +7,24 @@ let html = `
         <div class="card__wrapper-image">
             <img src='../images/andriyko-podilnyk-jFAG_ixCrsM-unsplash.jpg' >
         </div>
-        <div class="card__wrapper-info"></div>
-        <div class="card__wrapper-socialmedia"></div>
+        <div class="card__wrapper-info">
+            <p>Christina Everleen</p>
+            <p>Human Resources Expert</p>
+        </div>
+        <div class="card__wrapper-socials">
+            <div class="socials-facebook">
+                <img src="../images/facebook-icon.png">
+                <p>facebook</p>
+            </div>
+            <div class="socials-instagram">
+                <img src="../images/instagram-icon.png">
+                <p>facebook</p>
+            </div>
+            <div class="socials-twitter">
+                <img src="../images/twitter-icon.png">
+                <p>facebook</p>
+            </div>
+        </div>
     </div>
 `
 
@@ -30,17 +46,21 @@ let cssStyles = `
         flex-direction: column;
         align-items: center;
         justify-content: center;
+        text-align: center;
     }
 
     .card__wrapper-image {
-        width: 100px;
-        height: 100px;
+        width: 65px;
+        height: 65px;
+        border-radius: 50%;
+        border: 2px solid purple;
     }
 
     .card__wrapper-image img {
         width: 100%;
         height: 100%;
         object-fit: cover;
+        border-radius: 50%;
     }
 `;
 
@@ -55,5 +75,6 @@ root.style.cssText = `
     background-position: center;
 `;
 
-
+root.prepend(style);
+style.innerHTML = cssStyles;
 card.innerHTML = html;
